617ddd251ecb6d8c44d02cb69eebfeb50d782a9e
codnr <wnd75913@gmail.com>
2022-02-24 19:26:03 +0900

    Object

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const dayOfWeek = [
-    'mon', 'tue', 'wed', 'thu', 'fri', 'sat'
-]
+const player = {
+    name: "codnr",
+    points: 250000,
+    fat: false,
+}
 
-//Get Item from Array
-console.log(dayOfWeek[4]);
+//객체의 구성요소를 수정하거나 더할 수 있다.
+player.lastName = "lee";
+player.points = player.points + 5;
 
-dayOfWeek.push("sun");
+
+console.log(player);
+
+console.log(player.name);
+//두 코드 다 같은 역할 
+console.log(player["name"]);
+
+
+console.log(player.points);

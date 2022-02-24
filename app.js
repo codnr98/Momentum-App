4a990cb7ead3db5d2f05e7d63b04858519e829b5
codnr <wnd75913@gmail.com>
2022-02-24 18:57:05 +0900

    Array

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
-const a = 5;
-const b = 5;
-// javascript의 variable에서는 공백대신 공백 다음단어의 첫 알파벳을 대문자로 표기한다.
-let myName = 'codnr';
-// let은 값을 변경할 수 있다.
+const dayOfWeek = [
+    'mon', 'tue', 'wed', 'thu', 'fri', 'sat'
+]
 
+//Get Item from Array
+console.log(dayOfWeek[4]);
 
-console.log(a + b);
-console.log(a + b);
-console.log(a + b);
-
-myName = 'chaewook'
-
-console.log('Hello ' + myName);
-
+dayOfWeek.push("sun");

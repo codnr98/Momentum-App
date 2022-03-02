18a033fdb4a44b779b4835706c28c26ec28b8dec
codnr <wnd75913@gmail.com>
2022-03-02 22:50:57 +0900

    localStorage.setItem을 사용해서 local storage에 username저장

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 const logninForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"
 
 function onLoginSubmit(event) { //function의 argument에 아무것도 입력하지 않으면 어떤 정보도 받지 않겠다는 뜻 
     event.preventDefault(); // 누군가 form을 submit하면 브라우저는 기본동작(페이지를 새로고침) 하도록 되어있다. preventDefault()을 추가하면서 그 기본동작을 막고있다.
     const username = loginInput.value;
-    logninForm.classList.add("hidden");
-    greeting.innerText = `Hello ${username}!`; //innerText는 변수라서 =을 사용
-    greeting.classList.remove("hidde"); 
+    logninForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`; //innerText는 변수라서 =을 사용
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 logninForm.addEventListener("submit", onLoginSubmit);
